32f010f5953e19d663d065ca8dd8e2db7a58fe90
Pascal Sauer <email@cubexs.dev>
2025-10-31 17:28:56 +0100

    Refactor and add fallback values

--- backend/src/utilities/environment.ts
+++ backend/src/utilities/environment.ts
@@ -1,25 +1,41 @@
 interface Environments {
 	NODE_ENV: string
+	NODE_PORT: number
 }
 
 export const environments: Environments = {
-	NODE_ENV: retrieveEnvironmentValue('NODE_ENV')
+	NODE_ENV: retrieveEnvironmentStringValue('NODE_ENV', 'local'),
+	NODE_PORT: retrieveEnvironmentNumberValue('NODE_PORT', 3000)
 }
 
-function retrieveEnvironmentValue(key: string): string {
+function retrieveEnvironmentStringValue(key: string, fallback: string): string {
 	const envValue = process.env[key]
 	if (envValue === undefined) {
 		console.error(`Environment ${key} not found`)
-		return ''
+		return fallback
 	}
 	return envValue
 }
+function retrieveEnvironmentNumberValue(key: string, fallback: number): number {
+	const envValue = process.env[key]
+	if (envValue === undefined) {
+		console.error(`Environment ${key} not found`)
+		return fallback
+	}
 
-function retrieveEnvironmentValueAsArray(key: string): string[] {
+	const parsedValue = parseInt(envValue)
+	if (isNaN(parsedValue)) {
+		return fallback
+	}
+
+	return parsedValue
+}
+
+function retrieveEnvironmentStringArrayValue(key: string, fallback: string[]): string[] {
 	const envValues = process.env[key]
 	if (envValues === undefined) {
 		console.error(`Environment ${key} not found`)
-		return []
+		return fallback
 	}
 
 	return envValues.split(',')
