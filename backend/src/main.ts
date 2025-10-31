cbb16211026153aa354980f49db7229e8249288e
Pascal Sauer <email@cubexs.dev>
2025-10-31 17:06:06 +0100

    Start socket-io server on start up

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,50 @@
 import 'dotenv/config'
 
 import Fastify from 'fastify'
+import { Server as SocketIOServer } from 'socket.io'
 import { pinoLoggerConfig } from './utilities'
+
+declare module 'fastify' {
+	interface FastifyInstance {
+		io: SocketIOServer
+	}
+}
 
 const fastify = Fastify({
 	logger: pinoLoggerConfig
+})
+
+const isLocalEnv = process.env.NODE_ENV === 'local'
+
+const io = new SocketIOServer(
+	fastify.server,
+	isLocalEnv
+		? {
+				cors: {
+					origin: '*'
+				}
+			}
+		: undefined
+)
+
+fastify.decorate('io', io)
+
+fastify.addHook('onReady', async () => {
+	fastify.log.info('Socket-io is ready!')
+	fastify.io.on('connection', (socket) => {
+		fastify.log.info({ id: socket.id }, 'socket connected')
+
+		socket.on('ping', () => socket.emit('pong'))
+
+		socket.on('disconnect', () => {
+			fastify.log.info({ id: socket.id }, 'socket disconnected')
+		})
+	})
+})
+
+fastify.addHook('onClose', (instance, done) => {
+	instance.io.close()
+	done()
 })
 
 fastify.get('/', function (req, reply) {
