bbee0d8785f465647a9b8418cec90f9240b36f5b
Pascal Sauer <email@cubexs.dev>
2025-11-01 15:12:26 +0100

    Move "Game" into "Lobby"-component

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,67 +1,11 @@
-import { useState, type FC } from 'react'
-import { Button, Stack, Typography } from '@mui/material'
-import { Board } from './components/board'
-import { createNewGameMap, cloneGameMap } from './components/game-map/map-utilities'
-import type { GameMap } from './components/game-map/model'
-import { playerSymbols } from './components/game-map/player-symbols'
-import { checkForWin } from './components/game-map/check-for-win'
+import { type FC } from 'react'
+import { Navbar } from './components'
 
 export const App: FC = () => {
-	const [currentRound, setCurrentRound] = useState<number>(0)
-	const [gameMap, setGameMap] = useState<GameMap[][]>(createNewGameMap())
-	const [hasWon, setHasWon] = useState<boolean>(false)
-
-	const handleClick = (gameRow: number, gameCol: number) => {
-		return (fieldRow: number, fieldCol: number) => {
-			const nextGameMap = cloneGameMap(gameMap)
-			const currentField = nextGameMap[gameRow][gameCol]
-			currentField.board[fieldRow][fieldCol] = playerSymbols[currentRound % 2]
-			currentField.active = false
-
-			console.log({ gameRow, gameCol, fieldRow, fieldCol, currentField })
-			if (checkForWin(currentField.board, fieldRow, fieldCol, currentRound)) {
-				setGameMap(nextGameMap)
-				setHasWon(true)
-				return
-			}
-
-			nextGameMap[fieldRow][fieldCol].active = true
-			setGameMap(nextGameMap)
-			setCurrentRound((prevRound) => prevRound + 1)
-		}
-	}
-
-	const handleGameReset = () => {
-		setCurrentRound(0)
-		setGameMap(createNewGameMap())
-		setHasWon(false)
-	}
-
 	return (
-		<Stack alignItems="center" justifyContent="center" spacing={8} height="100vh">
-			<Stack>
-				<Typography variant="h1">
-					Tic Tac Toe<sup>2</sup>
-				</Typography>
-				<Typography variant="h3" textAlign="center" height={64}>
-					{!hasWon ? '' : `${playerSymbols[currentRound % 2]} has won the Game!`}
-				</Typography>
-			</Stack>
-			{gameMap.map((row, rowIndex) => (
-				<Stack direction="row" spacing={8} key={rowIndex}>
-					{row.map((col, colIndex) => (
-						<Board
-							key={colIndex}
-							disabled={!col.active}
-							handleClick={handleClick(rowIndex, colIndex)}
-							usedFieldsMap={col.board}
-						/>
-					))}
-				</Stack>
-			))}
-			<Button variant="outlined" onClick={handleGameReset}>
-				Reset Game
-			</Button>
-		</Stack>
+		<>
+			<Navbar />
+			App
+		</>
 	)
 }
---
+++ frontend/src/views/lobby/Lobby.tsx
@@ -0,0 +1,70 @@
+import { useState, type FC } from 'react'
+import { Button, Stack, Typography } from '@mui/material'
+import {
+	Board,
+	checkForWin,
+	createNewGameMap,
+	cloneGameMap,
+	type GameMap,
+	playerSymbols
+} from '../../components'
+
+export const Lobby: FC = () => {
+	const [currentRound, setCurrentRound] = useState<number>(0)
+	const [gameMap, setGameMap] = useState<GameMap[][]>(createNewGameMap())
+	const [hasWon, setHasWon] = useState<boolean>(false)
+
+	const handleClick = (gameRow: number, gameCol: number) => {
+		return (fieldRow: number, fieldCol: number) => {
+			const nextGameMap = cloneGameMap(gameMap)
+			const currentField = nextGameMap[gameRow][gameCol]
+			currentField.board[fieldRow][fieldCol] = playerSymbols[currentRound % 2]
+			currentField.active = false
+
+			console.log({ gameRow, gameCol, fieldRow, fieldCol, currentField })
+			if (checkForWin(currentField.board, fieldRow, fieldCol, currentRound)) {
+				setGameMap(nextGameMap)
+				setHasWon(true)
+				return
+			}
+
+			nextGameMap[fieldRow][fieldCol].active = true
+			setGameMap(nextGameMap)
+			setCurrentRound((prevRound) => prevRound + 1)
+		}
+	}
+
+	const handleGameReset = () => {
+		setCurrentRound(0)
+		setGameMap(createNewGameMap())
+		setHasWon(false)
+	}
+
+	return (
+		<Stack alignItems="center" justifyContent="center" spacing={8} height="100vh">
+			<Stack>
+				<Typography variant="h1">
+					Tic Tac Toe<sup>2</sup>
+				</Typography>
+				<Typography variant="h3" textAlign="center" height={64}>
+					{!hasWon ? '' : `${playerSymbols[currentRound % 2]} has won the Game!`}
+				</Typography>
+			</Stack>
+			{gameMap.map((row, rowIndex) => (
+				<Stack direction="row" spacing={8} key={rowIndex}>
+					{row.map((col, colIndex) => (
+						<Board
+							key={colIndex}
+							disabled={!col.active}
+							handleClick={handleClick(rowIndex, colIndex)}
+							usedFieldsMap={col.board}
+						/>
+					))}
+				</Stack>
+			))}
+			<Button variant="outlined" onClick={handleGameReset}>
+				Reset Game
+			</Button>
+		</Stack>
+	)
+}
