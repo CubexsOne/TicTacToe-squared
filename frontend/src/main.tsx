6fee348683cc4a1699824320699d36ecf942912b
Pascal Sauer <email@cubexs.dev>
2025-11-01 14:48:51 +0100

    Remove data router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router/dom'
 import { ThemeProvider } from '@mui/material/styles'
 
 import '@fontsource-variable/roboto'
 import './index.css'
 
-import './assets/i18n/i18n'
-import router from './router'
+import './i18n/i18n'
 import { theme } from './theme'
+import { App } from './views/app'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<ThemeProvider theme={theme}>
-			<RouterProvider router={router} />
+			<App />
 		</ThemeProvider>
 	</StrictMode>
 )
