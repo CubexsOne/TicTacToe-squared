c9e1b517259f008561f11ec5f70e0ae5632a905a
Pascal Sauer <email@cubexs.dev>
2025-11-02 16:28:15 +0100

    Add events

--- frontend/src/utilities/socket-events.ts
+++ frontend/src/utilities/socket-events.ts
@@ -1,7 +1,9 @@
 export const outgoingEvents = {
-	CREATE_GAME: 'create_game'
+	CREATE_GAME: 'create_game',
+	REQUEST_GAME: 'request_game'
 }
 
 export const incomingEvents = {
-	GAME_CREATED: 'game_created'
+	GAME_CREATED: 'game_created',
+	RECEIVE_GAME: 'receive_game'
 }
