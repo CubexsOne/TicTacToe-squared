a0c231020c177666fc2090abb23b24ed6e7641b3
Pascal Sauer <email@cubexs.dev>
2025-11-02 14:49:40 +0100

    Implement 'GameManager' to create and manage games

---
+++ backend/src/game/manager.ts
@@ -0,0 +1,52 @@
+import { randomUUID, UUID } from 'crypto'
+
+type Player = {
+	socketId: string
+	playername: string
+	symbol: string
+}
+
+type Game = {
+	id: UUID
+	player: Player[]
+	board: object
+}
+
+type Games = Game[]
+
+export class GameManager {
+	private static instance: GameManager | null = null
+
+	public static getInstance(): GameManager {
+		if (GameManager.instance === null) {
+			GameManager.instance = new GameManager()
+		}
+
+		return GameManager.instance
+	}
+
+	public static get Instance(): GameManager {
+		return GameManager.getInstance()
+	}
+
+	private games: Games = []
+
+	private constructor() {}
+
+	public create(socketId: string, playername: string): Game {
+		const firstPlayer: Player = {
+			socketId,
+			playername,
+			symbol: 'X'
+		}
+
+		const game: Game = {
+			id: randomUUID(),
+			player: [firstPlayer],
+			board: {}
+		}
+
+		this.games.push(game)
+		return game
+	}
+}
