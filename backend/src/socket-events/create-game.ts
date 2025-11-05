8cf25ee8b81bef8548bf4e848229f33e0d97edf4
Pascal Sauer <email@cubexs.dev>
2025-11-05 14:21:27 +0100

    Using socket.io rooms

--- backend/src/socket-events/create-game.ts
+++ backend/src/socket-events/create-game.ts
@@ -8,6 +8,11 @@ export const GAME_CREATED = 'game_created'
 export const handleCreateGame = (socket: Socket, playername: string) => {
 	logger.info(`Event received: ${CREATE_GAME}`)
 	const gameId = GameManager.Instance.create(socket.id, playername)
+
+	// Move player to game room
+	socket.join(gameId)
+	socket.leave(socket.id)
+
 	socket.emit(GAME_CREATED, gameId)
 	logger.info({ gameId }, `Event emitted: ${GAME_CREATED}`)
 }
