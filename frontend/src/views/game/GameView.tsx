4c1e5caa9231654d20bd5c0b3bf1af768fe0b359
Pascal Sauer <email@cubexs.dev>
2025-11-05 13:27:44 +0100

    Show game id to copy if just one player is connected

--- frontend/src/views/game/GameView.tsx
+++ frontend/src/views/game/GameView.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState, type FC } from 'react'
-import { Stack } from '@mui/material'
+import { Grid, Stack } from '@mui/material'
 import { useParams } from 'react-router'
+
 // import {
 // 	Board,
 // 	checkForWin,
@@ -11,7 +12,8 @@ import { useParams } from 'react-router'
 // } from '../../components'
 import { getSocket, incomingEvents, outgoingEvents } from '../../utilities'
 import type { Game } from './model'
-import { Board } from '../../components'
+import { Board, GameIdCopyField } from '../../components'
+import { playerSymbols } from '../../utilities/game'
 
 export const GameView: FC = () => {
 	const { id } = useParams()
@@ -47,21 +49,32 @@ export const GameView: FC = () => {
 		}
 	}
 
+	console.log({
+		playerId: io.id,
+		currentRound: game?.currentRound,
+		activePlayer: playerSymbols[game?.currentRound || 0 % 2]
+	})
+
 	return (
-		<Stack alignItems="center" justifyContent="center" spacing={8} className="h-full">
-			{game &&
-				game?.board.map((row, rowIndex) => (
-					<Stack direction="row" spacing={8} key={rowIndex}>
-						{row.map((col, colIndex) => (
-							<Board
-								key={colIndex}
-								disabled={!col.active}
-								handleClick={handleClick(rowIndex, colIndex)}
-								usedFieldsMap={col.board}
-							/>
-						))}
-					</Stack>
-				))}
-		</Stack>
+		<>
+			<Grid className="h-1/10 px-8 py-4" container spacing={2}>
+				{game && game.player.length < 2 && <GameIdCopyField gameId={id || ''} />}
+			</Grid>
+			<Stack alignItems="center" justifyContent="center" spacing={8} className="h-8/10">
+				{game &&
+					game?.board.map((row, rowIndex) => (
+						<Stack direction="row" spacing={8} key={rowIndex}>
+							{row.map((col, colIndex) => (
+								<Board
+									key={colIndex}
+									disabled={!col.active}
+									handleClick={handleClick(rowIndex, colIndex)}
+									usedFieldsMap={col.board}
+								/>
+							))}
+						</Stack>
+					))}
+			</Stack>
+		</>
 	)
 }
