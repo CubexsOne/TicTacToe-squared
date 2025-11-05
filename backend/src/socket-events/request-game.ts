5965cb65f2be66d4affc8539041096099c4ff15c
Pascal Sauer <email@cubexs.dev>
2025-11-05 14:21:52 +0100

    Add ToDo and update logging for just the gameId

--- backend/src/socket-events/request-game.ts
+++ backend/src/socket-events/request-game.ts
@@ -6,8 +6,9 @@ export const REQUEST_GAME = 'request_game'
 export const RECEIVE_GAME = 'receive_game'
 
 export const handleRequestGame = (socket: Socket, id: Game['id']) => {
+	// TODO: Player check?
 	logger.info(`Event received: ${REQUEST_GAME}`)
 	const game = GameManager.Instance.getGameById(id)
 	socket.emit(RECEIVE_GAME, game)
-	logger.info({ game }, `Event emitted: ${RECEIVE_GAME}`)
+	logger.info({ gameId: game.id }, `Event emitted: ${RECEIVE_GAME}`)
 }
